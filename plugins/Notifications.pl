3ab50ce8bf8740280465cfb52e09f87aaa847a64
joe <joe@u13.net>
2012-02-01 17:33:34 +0000

    Notifications plugin for libnotify for song events

---
+++ plugins/Notifications.pl
@@ -0,0 +1,64 @@
+#!/usr/bin/perl
+
+# This file is part of MicroJuke (c) 2012 Joe Gillotti.
+# 
+# MicroJuke is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# MicroJuke is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with MicroJuke.  If not, see <http://www.gnu.org/licenses/>.
+
+package MicroJuke::Plugin::Notifications;
+
+use strict;
+use warnings;
+
+my $have_notify;
+
+BEGIN {
+	eval {
+		require Gtk2::Notify;
+		Gtk2::Notify->import();
+	};
+	if ($@) {
+		$have_notify = 0;
+	}
+	else {
+		$have_notify = 1;
+	}
+
+}
+
+sub new {
+	print "Notifications module loaded\n";
+	my $self = $_[1];
+	bless $self;
+
+	if ($have_notify == 0) {
+		warn "I don't have LibNotify. Notifications won't work. :P\n";
+	}
+	else {
+		Gtk2::Notify->init('MicroJuke');
+	}
+
+	$self;
+}
+
+sub onSongStart {
+	my $self = shift;
+	return unless $have_notify;
+	my $notif = Gtk2::Notify->new(
+		"Now playing",
+		$self->{play}->{gstate}->{title}." by ".$self->{play}->{gstate}->{artist}
+	);
+	$notif->show;
+}
+
+1;
